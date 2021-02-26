6d81c64cdf1c9676624a9ff90439476f65350408
Gabriel044 <gabgar4@gmail.com>
2021-02-26 06:02:34 +0000

    Word reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+#define OUT 0
+#define IN 1
+
+char * reverse(int length, char word[50]){
+    char temp;
+    for(int k = 0; k < length/2; k++){
+        if(k != length - (1 + k)){
+            temp = word[k];
+            word[k] = word[length - (1 + k)];
+            word[length - (1 + k)] = temp;
+        }
+    }
+    return word;
+}
+
+int main(int argc, char **argv){
     // Place your magic here
+    //getchar() -> Reads a character from stdin
+    int i, wordLength ;
+    i = 0;
+    wordLength = 0;
+    char *input = malloc(50*sizeof(char));
+    printf("%s",input);
+    char c;
+    
+    while((c = getchar()) != EOF){
+        
+        if(c == '\n' || c == ' ' || c == '\t'){
+           printf("%s ",reverse(wordLength,input));
+            wordLength = 0;
+            i = 0;
+           
+        }else {
+            input[i] = c;
+            i ++;
+            wordLength ++;
+        }
+    }
+    printf("\n");
     return 0;
 }

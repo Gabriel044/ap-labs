c985a4a1f414a89eaf6e7acbb629d56bf35bac8e
Gabriel044 <gabgar4@gmail.com>
2021-03-16 20:40:55 +0000

    Month_day

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int yearday, int *pmonth, int *pday);
 
-int main() {
+struct Month {
+    char *name;
+    int number;
+};
+
+const struct Month months[] = {{"Jan",1},{"Feb",2},{"Mar",3},{"Apr",4},
+                                {"May",5},{"Jun",6},{"Jul",7},{"Aug",8},
+                                {"Sep",9},{"Oct",10},{"Nov",11},{"Dec",12}};
+
+int main(int argc, char **args) {
+    int year = atoi(args[1]);
+    int day = atoi(args[2]);
+    if(day < 1 || day > 365){
+        printf("There is not that many days in a year\n");
+        return 0;
+    }
+    int month; 
+    int monthday;
+    month_day(day, &month, &monthday);
+    printf("%s %d, %d\n", months[month].name, monthday, year);
     return 0;
+}
+
+void month_day(int yearday, int *pmonth, int *pday){
+    *pmonth = floor(yearday/31);
+    *pday = yearday%30 + 1;
 }

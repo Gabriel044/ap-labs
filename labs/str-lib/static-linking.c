0b805d449ccecc2a2802c4cf76413dfee867c068
Gabriel044 <gabgar4@gmail.com>
2021-03-22 05:50:55 +0000

    Str-lib solved

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+int compareStrings(char *s1, char *s2);
+
+int main(int argc, char **argv) {
+
+    if(argc < 4){
+        printf("You need at least three arguments to run this program.\n");
+        return 0;
+    }
+    char *first_word = argv[2];
+    char *second_word = argv[3];
+
+    if(compareStrings(argv[1],"-add")){
+        printf("Initial length:   %d\n",mystrlen(first_word));
+        char *merged_word = mystradd(first_word,second_word);
+        printf("New String:       %s\n",merged_word);
+        printf("New length:       %d\n",mystrlen(merged_word));
+    }else if(compareStrings(argv[1],"-find")){
+        int position = mystrfind(first_word,second_word);
+        if(position < 0){
+            printf("The substring was not found in the main stirng,\n");
+        }else{
+            printf("['%s'] string was found at [%d] position\n",second_word,position);
+        }
+    }else{
+        printf("Request not recognized\n");
+    }
+
     return 0;
+}
+
+int compareStrings(char *s1, char *s2){
+    char *base = s1;
+    char *compare = s2;
+    for (; *base != 0 && *compare != 0; base++, compare++){
+        if(*base != *compare) return 0;
+    }
+    return (*base == 0 && *compare == 0) ? 1 : 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,44 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int lenght = 0;
+    for(char *c = str; *c != 0; c++){
+        lenght++;
+    }
+    return lenght;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int pos = 0, new_length = mystrlen(origin)+mystrlen(addition)+1;
+    char *new_string = (char* ) malloc(new_length);
+    
+    for(char *c = origin; *c != 0; c++){
+        new_string[pos++] = *c;
+    }
+    for(char *c = addition; *c != 0; c++){
+        new_string[pos++] = *c;
+    }
+    new_string[pos] = 0; 
+    return new_string;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    char *current = substr;
+    int sub_length = mystrlen(substr),
+        counter = 0, 
+        current_position = 0,
+        starting_position = -1;
+    for(char *c = origin; *c != 0; c++){
+        if(*c == *current++){
+            counter++;
+            if(starting_position < 0) starting_position = current_position;
+            if(counter == sub_length) return starting_position;
+        }else{
+            counter == 0;
+            starting_position = -1;
+            current = &substr[0];
+        }
+        current_position ++;
+    }
+    return -1;
 }
